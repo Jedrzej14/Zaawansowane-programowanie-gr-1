acc8ccdfd3a91acea2cf4885b5a23073c49bcf2f
Jedrzej14 <jedrzej.klus@gmail.com>
2024-11-16 10:14:22 +0100

    Dodano zadanie 1 // 2

---
+++ lab_1/zad_2.py
@@ -0,0 +1,23 @@
+def wyswietl_imiona(imiona):
+    for imie in imiona:
+        print(imie)
+# Przykład użycia
+lista_imion = ['Anna', 'Jan', 'Michał', 'Kasia', 'Piotr']
+wyswietl_imiona(lista_imion)
+
+
+# %%
+
+def multiply_by_two_for_loop(numbers):
+    result = []
+    for number in numbers:
+        result.append(number * 2)
+    return result
+
+# Przykład użycia
+numbers = [1, 2, 3, 4, 5]
+print(multiply_by_two_for_loop(numbers))
+
+# %%
+def pomnozone_przez_dwa(numbers):
+    return()

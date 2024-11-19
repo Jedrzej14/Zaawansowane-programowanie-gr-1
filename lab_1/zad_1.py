29ea530c31972bfb5f4e33ea37f9d908bd551518
Jedrzej14 <jedrzej.klus@gmail.com>
2024-11-19 09:54:15 +0100

    Dodano zadania 1-6

---
+++ lab_1/zad_1.py
@@ -0,0 +1,7 @@
+# Zad_1
+def imie_nazwisko(name, surname):
+    return 'Cześć {} {}'.format(name, surname)
+name = 'Jedrzej'
+surname = 'Klus'
+wynik = imie_nazwisko(name, surname)
+print(wynik)
---
+++ lab_1/zad_5.py
@@ -0,0 +1,8 @@
+# Zad_5
+
+def dwa_argumenty(lista, liczba):
+    return liczba in lista
+lista = [1,2,3]
+liczba = 2
+wynik = dwa_argumenty(lista, liczba)
+print(wynik)
